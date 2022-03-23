7dc75886dcf1a36d20c7cafe300c88f31204e102
Dacomis <albei.andrei@gmail.com>
2022-03-23 19:31:19 +0200

    Add clouds on header and footer and finish footer

--- src/App.tsx
+++ src/App.tsx
@@ -21,20 +21,26 @@ function App() {
         <img
           className="min-w-screen rotate-180 overflow-hidden"
           src={require("./images/clouds.png")}
-          alt="anetaBTC Angel logo"
+          alt="clouds header"
         />
-        <img
-          className="absolute top-0 w-48 p-3"
-          src={require("./images/anetaBTC_logo_text_black.png")}
-          alt="anetaBTC Angel logo"
-        />
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://anetabtc.io/"
+        >
+          <img
+            className="absolute top-0 w-48 p-3"
+            src={require("./images/anetaBTC_logo_text_black.png")}
+            alt="anetaBTC logo"
+          />
+        </a>
       </header>
 
       <div className="flex justify-center pt-16 pb-40">
         <img
           className={`${animationEffect && "animate-bounce"} w-3/12`}
           src={require("./images/anetaBTC_angel1.png")}
-          alt="anetaBTC logo"
+          alt="anetaBTC angel logo"
         />
 
         <div
@@ -77,7 +83,7 @@ function App() {
 
       <div>{Boolean(rewards) && <LineAndBarGraph rewards={rewards} />}</div>
 
-      <div className="m-auto mt-24 flex w-10/12">
+      <div className="m-auto mt-24 mb-20 flex w-10/12">
         <TotalADAStaked />
         <Stakers />
       </div>
@@ -86,18 +92,74 @@ function App() {
         <img
           className="min-w-screen overflow-hidden"
           src={require("./images/clouds.png")}
-          alt="anetaBTC Angel logo"
+          alt="anetaBTC logo"
         />
-        <div className="min-w-screen h-32 bg-white">
-          <img
-            className="absolute bottom-0 w-40 p-3"
-            src={require("./images/anetaBTC_logo_text_black.png")}
-            alt="anetaBTC Angel logo"
-          />
-          <div className="w-22">
-            anetaBTC is a decentralized, secure protocol that allows users to
-            unlock the value of their Bitcoin on Ergo and Cardano and the rest
-            of the footer.
+        <div className="min-w-screen flex h-24 justify-between bg-white">
+          <div className="ml-2">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://anetabtc.io/"
+            >
+              <img
+                className="absolute bottom-24 w-40 p-1"
+                src={require("./images/anetaBTC_logo_text_black.png")}
+                alt="anetaBTC logo"
+              />
+            </a>
+            <div className="ml-2 w-96 px-2">
+              anetaBTC is a decentralized, secure protocol that allows users to
+              unlock the value of their Bitcoin on Ergo and Cardano.
+            </div>
+          </div>
+          <div className="justify-self-end px-5">
+            <div className="mb-2 flex">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://discord.gg/anetabtc"
+              >
+                <img
+                  className="ml-7 h-10 w-10"
+                  src={require("./images/discord.png")}
+                  alt="discord logo"
+                />
+              </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://twitter.com/anetaBTC"
+              >
+                <img
+                  className="ml-7 h-10 w-10"
+                  src={require("./images/twitter.png")}
+                  alt="twitter logo"
+                />
+              </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://medium.com/@anetaBTC"
+              >
+                <img
+                  className="ml-7 h-10 w-10"
+                  src={require("./images/medium.png")}
+                  alt="medium logo"
+                />
+              </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://t.me/anetaBTC"
+              >
+                <img
+                  className="ml-7 h-10 w-10"
+                  src={require("./images/telegram.png")}
+                  alt="telegram logo"
+                />
+              </a>
+            </div>
+            <span className="px-2">anetaBTC is a Singapore based DAO</span>
           </div>
         </div>
       </footer>
