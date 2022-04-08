01a4dbc95ee66146a87203d764c820be8ff54bd9
Dacomis <albei.andrei@gmail.com>
2022-04-08 21:03:32 +0300

    Remove api cache

--- index.js
+++ index.js
@@ -8,12 +8,10 @@ import {
 import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
-// import apicache from "apicache";
 
-const PORT = get("PORT") || 5000;
+const PORT = get("PORT");
 
 const app = express();
-// let cache = apicache.middleware;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
